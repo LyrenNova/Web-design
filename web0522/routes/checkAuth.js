dad9b5f371624bc1a5fecd9b32f11929589b9bda
LyrenNova <49513731+LyrenNova@users.noreply.github.com>
2019-05-23 17:16:04 +0800

    0523

--- web0522/routes/checkAuth.js
+++ web0522/routes/checkAuth.js
@@ -4,9 +4,9 @@ var router = express.Router();
 //處理GET, POST, PUT, DELETE等所有請求
 router.all('/', function(req, res, next) {
     //檢查是否有session註記
-    var userno = req.session.userno;
+    var id = req.session.empno;
     
-    if(userno===null || userno===undefined){
+    if(id===null || id===undefined){
         res.render('unAuth');  //導向無權限畫面        
     }else{
         next();  //執行在app.use()中, 串接在checkAuth之後的函式 
